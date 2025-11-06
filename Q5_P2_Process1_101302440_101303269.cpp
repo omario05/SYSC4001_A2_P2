2d09c835d44f458cbd42bd509bcab6e402768a4c
Omar Elhogaraty <elhogaratyomar@gmail.com>
2025-11-06 11:26:26 -0500

    Q4 and Q5 template
    Solution coming soon

---
+++ Q5_P2_Process1_101302440_101303269.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/shm.h>
+#include <semaphore.h>
+
+int main(void)
+{
+    
+}
